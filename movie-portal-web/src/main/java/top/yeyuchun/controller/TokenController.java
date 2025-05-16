8bc7d6e3cecca76b8ce562fe609d0fe961e0ef55
Arthur <772200522@qq.com>
2025-05-16 13:49:57 +0800

    解决jwt认证bug

--- movie-portal-web/src/main/java/top/yeyuchun/controller/TokenController.java
+++ movie-portal-web/src/main/java/top/yeyuchun/controller/TokenController.java
@@ -1,7 +1,10 @@
 package top.yeyuchun.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import top.yeyuchun.result.Result;
 import top.yeyuchun.service.TokenService;
 
